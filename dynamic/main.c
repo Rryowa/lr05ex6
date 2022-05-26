5db6cb7f29632cfacdf3e0b0953af7494e58e288
Rryowa <rryowa@gmail.com>
2022-05-26 20:19:45 +0300

    dynamic library created

---
+++ dynamic/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "test.h"
+
+int main(){
+	printf("chislo: \n");
+	int a;
+	scanf("%d", &a);
+
+	if (check(a)){
+		printf(">0\n");
+	}
+	else{
+		printf("<=0\n");
+	}
+	return 0;
+}
