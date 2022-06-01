869cc9c23330f1075486baf0e5f0dee0371e8173
Rryowa <rryowa@gmail.com>
2022-06-01 12:31:00 +0300

    makefilel for multifunction program

--- dop1/main.c
+++ dop1/main.c
@@ -2,9 +2,7 @@
 #include <math.h>
 #include <stdlib.h>
 #include <string.h>
-
-int check(int a, int n);
-int ten(int a, int n, int len);
+#include "func.h"
 
 int main(){
 	printf("chislo: \n");
@@ -28,23 +26,3 @@ int main(){
 	return 0;
 }
 
-int check(int a, int n){
-	while (a > 0){
-		if (a % 10 >= n){
-			return 0;
-		}
-		a = a/10;
-		if (a <= 0){
-			return 1;
-		}
-	}
-}
-
-int ten(int a, int n, int len){
-	int z = 0;
-	for (int i = 0; i < len; i++){
-		z = z + (a % 10) * pow(n, i);
-		a = a / 10;
-	}
-	return z;
-}
